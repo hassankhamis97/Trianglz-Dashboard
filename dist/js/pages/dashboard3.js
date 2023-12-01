cc54a583086498f222d2f334aa87c182e124dcb8
Hassan <hassan97khamis@gmail.com>
2023-12-02 00:49:49 +0200

    handle details

--- dist/js/pages/dashboard3.js
+++ dist/js/pages/dashboard3.js
@@ -19,8 +19,8 @@ $(function () {
       labels: ['JUN', 'FEB', 'Mar', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
       datasets: [
         {
-          backgroundColor: '#007bff',
-          borderColor: '#007bff',
+          backgroundColor: '#234A71',
+          borderColor: '#234A71',
           data: [10, 20, 30, 25, 27, 25, 30, 40, 30, 30, 40, 50]
         },
         {
@@ -86,12 +86,12 @@ $(function () {
         type: 'line',
         data: [100, 120, 170, 167, 180, 177, 160],
         backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        pointBorderColor: '#007bff',
-        pointBackgroundColor: '#007bff',
+        borderColor: '#234A71',
+        pointBorderColor: '#234A71',
+        pointBackgroundColor: '#234A71',
         fill: false
-        // pointHoverBackgroundColor: '#007bff',
-        // pointHoverBorderColor    : '#007bff'
+        // pointHoverBackgroundColor: '#234A71',
+        // pointHoverBorderColor    : '#234A71'
       },
       {
         type: 'line',
--- docs/assets/js/pages/dashboard3.js
+++ docs/assets/js/pages/dashboard3.js
@@ -19,8 +19,8 @@ $(function () {
       labels: ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
       datasets: [
         {
-          backgroundColor: '#007bff',
-          borderColor: '#007bff',
+          backgroundColor: '#234A71',
+          borderColor: '#234A71',
           data: [1000, 2000, 3000, 2500, 2700, 2500, 3000]
         },
         {
@@ -86,12 +86,12 @@ $(function () {
         type: 'line',
         data: [100, 120, 170, 167, 180, 177, 160],
         backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        pointBorderColor: '#007bff',
-        pointBackgroundColor: '#007bff',
+        borderColor: '#234A71',
+        pointBorderColor: '#234A71',
+        pointBackgroundColor: '#234A71',
         fill: false
-        // pointHoverBackgroundColor: '#007bff',
-        // pointHoverBorderColor    : '#007bff'
+        // pointHoverBackgroundColor: '#234A71',
+        // pointHoverBorderColor    : '#234A71'
       },
       {
         type: 'line',
